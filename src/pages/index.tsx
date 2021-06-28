292fa7c92117fd46bd224e0334d193a690f3d93f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-28 13:04:37 -0400

    feat: change structure and add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,28 +10,33 @@ export default function Home() {
   const { highlightText, regexInput, regexFlags } = useContext(RegexContext);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.regex}>
-        <RegexInput />
-        <RegexFlagsInput />
-      </div>
-      <TextInput />
-      <div className={styles.regexInfo}>
-        <div>
-          <h3>Used Expression:</h3>
-          <p>
-            /{regexInput}/{regexFlags.join("")}
-          </p>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.regex}>
+          <RegexInput />
+          <RegexFlagsInput />
         </div>
-        <div>
-          <h3>Result: </h3>
-          <p
-            dangerouslySetInnerHTML={{ __html: highlightText }}
-            className={styles.highlight}
-          />
+        <TextInput />
+        <div className={styles.regexInfo}>
+          <div>
+            <h3>Used Expression:</h3>
+            <p>
+              /{regexInput}/{regexFlags.join("")}
+            </p>
+          </div>
+          <div>
+            <h3>Result: </h3>
+            <p
+              dangerouslySetInnerHTML={{ __html: highlightText }}
+              className={styles.highlight}
+            />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+      <footer className={styles.footer}>
+        <a href="#">Regex Helper</a> made by <a href="https://github.com/rmrantunes">@rmrantunes</a>
+      </footer>
+    </div>
     // Regex options modal // TextInput
   );
 }
